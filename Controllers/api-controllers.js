d0c8ff74c89a5c0db60aba868ff774c41d96f2e4
Andreas <andreasjr77@outlook.com>
2023-05-19 15:44:53 +0300

    Completed ticket13 Refactor

---
+++ Controllers/api-controllers.js
@@ -0,0 +1,6 @@
+const endpoints = require("../endpoints.json");
+
+exports.getEndpoints = async (req, res, next) => {
+  res.status(200).send(endpoints);
+  
+};
--- app.js
+++ app.js
@@ -20,8 +20,12 @@ const {
 
 const { getUsers } = require("./Controllers/users-controllers");
 
+const { getEndpoints } = require("./Controllers/api-controllers");
+
 const app = express();
 app.use(express.json());
+
+app.get("/api", getEndpoints);
 
 app.get("/api/topics", getTopics);
 
