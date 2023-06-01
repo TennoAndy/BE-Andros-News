9b459fab634525e03e22675c77a20fd8a6e47c6e
Andreas <andreasjr77@outlook.com>
2023-06-01 18:50:57 +0300

    ticket17 completed

--- Controllers/users-controllers.js
+++ Controllers/users-controllers.js
@@ -1,9 +1,27 @@
-const { selectUsers } = require("../models/users-models");
+const {
+  selectUsers,
+  selectUserByUsername,
+  checkUserExists,
+} = require("../models/users-models");
 
 exports.getUsers = async (req, res, next) => {
   try {
     const users = await selectUsers();
     res.status(200).send({ users });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getUserByUsername = async (req, res, next) => {
+  try {
+    const username = req.params.username;
+    const [, user] = await Promise.all([
+      checkUserExists(username),
+      selectUserByUsername(username),
+    ]);
+
+    res.status(200).send({ user });
   } catch (err) {
     next(err);
   }
--- models/users-models.js
+++ models/users-models.js
@@ -5,3 +5,20 @@ exports.selectUsers = async () => {
   const { rows } = await db.query(`SELECT * FROM users`);
   return rows;
 };
+
+exports.selectUserByUsername = async (username) => {
+  const { rows } = await db.query(`SELECT * FROM users WHERE username=$1`, [
+    username,
+  ]);
+  return rows[0];
+};
+
+exports.checkUserExists = async (username) => {
+  const { rows } = await db.query(
+    `SELECT username FROM users WHERE username=$1`,
+    [username]
+  );
+  if (!rows[0]) {
+    return Promise.reject({ code: 404, msg: "User Not Found!" });
+  }
+};
