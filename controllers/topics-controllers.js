5b75a53cd2a7f3249229acc090e4f36d69509253
Andreas <andreasjr77@outlook.com>
2023-06-03 19:11:50 +0300

    ticket22 completed

--- controllers/topics-controllers.js
+++ controllers/topics-controllers.js
@@ -1,7 +1,15 @@
-const { selectTopics } = require("../models/topics-models");
+const { selectTopics, insertTopic } = require("../models/topics-models");
 
 exports.getTopics = (req, res, next) => {
   selectTopics().then((topics) => res.status(200).send({ topics }));
 };
 
-
+exports.postTopic = async (req, res, next) => {
+  try {
+    const topic = req.body;
+    const newTopic = await insertTopic(topic);
+    res.status(201).send({ newTopic });
+  } catch (err) {
+    next(err);
+  }
+};
--- models/topics-models.js
+++ models/topics-models.js
@@ -1,6 +1,5 @@
 const db = require("../db/connection");
 
-
 exports.selectTopics = () => {
   return db.query(`SELECT * FROM topics`).then(({ rows }) => rows);
 };
@@ -11,8 +10,18 @@ exports.checkTopicExists = async (topic) => {
   const { rows } = await db.query(`SELECT * FROM topics WHERE slug=$1`, [
     topic,
   ]);
-  if (!rows[0]) { 
+  if (!rows[0]) {
     return Promise.reject({ code: 404, msg: "Topic Not Found!" });
   }
 };
 
+exports.insertTopic = async ({ slug, description }) => {
+  if (!slug || !description) {
+    return Promise.reject({ code: 400, msg: "Missing Required Fields!" });
+  }
+  const { rows } = await db.query(
+    `INSERT INTO topics (slug,description) VALUES ($1, $2) RETURNING *`,
+    [slug, description]
+  );
+  return rows[0];
+};
