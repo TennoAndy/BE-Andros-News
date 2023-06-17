f93e17c6980c5790c7b5a5c725d93b02721fd637
Andreas <andreasjr77@outlook.com>
2023-06-17 12:38:50 +0300

    Cors setup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const apiRouter = require("./routes/api-router");
+const cors = require("cors");
 
 const {
   handlePsqlErrors,
@@ -9,6 +10,7 @@ const {
 } = require("./controllers/errors-controllers");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
