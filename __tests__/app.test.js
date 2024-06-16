8d50a1f675183a5585e3f35a9bcee1cf20b45fc9
Andreas <andreasjr77@outlook.com>
2024-06-16 10:39:46 +0300

    not used import removed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const request = require("supertest");
 const app = require("../app");
 const data = require("../db/data/test-data");
 const seed = require("../db/seeds/seed");
-const articles = require("../db/data/test-data/articles");
+
 
 beforeEach(() => {
   return seed(data);
